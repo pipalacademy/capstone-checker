22099a43f21babda876a579fc2ba3164d176c4fc
Kaustubh M <37668193+nikochiko@users.noreply.github.com>
2023-05-30 07:57:11 +0000

    Add changes to validate deployment_type and deployment_options

--- capstone_checker/validator.py
+++ capstone_checker/validator.py
@@ -1,6 +1,6 @@
 from typing import Any
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field, root_validator
 
 
 class CheckModel(BaseModel):
@@ -22,5 +22,28 @@ class ProjectModel(BaseModel):
     short_description: str
     description: str
     tags: list[str]
-    is_published: bool | None = None
+    project_type: str
+    deployment_type: str
+    deployment_options: dict[str, Any] = Field(default_factory=dict)
     tasks: list[TaskModel]
+
+    @root_validator(pre=True)
+    def split_deployment_type_into_key_and_options(cls, values):
+        if "deployment_type" not in values:
+            # default validation will catch this
+            return values
+
+        deployment_type = values["deployment_type"]
+        if isinstance(deployment_type, dict):
+            deployment_type_items = list(deployment_type.items())
+            if len(deployment_type_items) != 1:
+                raise ValueError(
+                    "deployment_type must either be a string or a dict with only one key"
+                )
+
+            deployment_type, deployment_options = deployment_type_items[0]
+            return dict(
+                values, deployment_type=deployment_type, deployment_options=deployment_options
+            )
+        else:
+            return values
